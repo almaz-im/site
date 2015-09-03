ec31ea82afd4e352852d15467646cafb5bfb3e29
site <almazrock@yandex.ru>
2015-09-03 19:26:56 +0300

    index.php

---
+++ index.php
@@ -0,0 +1,261 @@
+﻿<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>макет сайта</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+<div id="container" align="center">
+  <div id="welcome">
+    <div class="navigation">
+      <ul>
+        <li><a href="index.php">HOME</a></li>
+        <li><a href="#">ABOUT</a></li>
+        <li><a href="#">WORK</a></li>
+        <li><a href="#">BLOG</a></li>
+        <li><a href="#">CONTACT</a></li>
+      </ul>
+    </div>
+    <p style="font-family:Gotham, 'Helvetica Neue', Helvetica, Arial, sans-serif; position:absolute; color:#fff;" align="center">Hi there! We are the new kids on the block<br>and we build awesome websites and mobile apps</p>
+    <div class="big"><img src="img/Big button.jpg" width="293" height="70" alt=""/></div>
+    </div>
+  <div id="services">
+    <h1>SERVICES WE PROVIDE<hr style="width:88px; color:#fff;"></h1>
+    <p style="margin-bottom:70px;"><b>We are working with both individuals and businesses from all over the globe<br> to create awesome websites and applications.</b></p>
+    <table width="1100" border="0">
+      <tr align="center">
+        <td><img src="img/branding.jpg" width="124" height="121" alt=""/></td>
+        <td><img src="img/design.jpg" width="74" height="123" alt=""/></td>
+        <td><img src="img/development.jpg" width="159" height="126" alt=""/></td>
+        <td><img src="img/rocket science.jpg" width="91" height="138" alt=""/></td>
+      </tr>
+      <tr style="font-size:20px;">
+        <td>BRANDING</td>
+        <td>DESIGN</td>
+        <td>DEVELOPMENT</td>
+        <td>ROCKET SCIENCE</td>
+      </tr>
+      <tr>
+        <td><br>Lorem ipsum dolor sit
+          amet. consectetuer
+          adipiscing elit. sed diam
+        nonummy nibh.</td>
+        <td>Sed ut perspiciatis unde
+          omnis iste natus error sit
+        voluptatem</td>
+        <td><br>At vero eos et<br>
+          accusamus et iusto odio
+          dignissimos qui blanditiis
+        praesentium</td>
+        <td>Et harum quidem rerum<br>
+          est et expedita distinctio.
+        Nam libero tempore.</td>
+      </tr>
+    </table>
+    </div>
+  <div id="team">
+    <h2>MEET OUR TEAM<hr style="width:90px; color:#d6e1e1;"></h2>
+    <p style="margin-bottom:70px; font-size:12px;"><b>We are a small team of designers and developers, who help brands with big ideas.</b></p>
+    <table width="1100" border="0">
+      <tr>
+        <td><img src="img/James.jpg" width="200" height="200" alt=""/></td>
+        <td><img src="img/Peter.jpg" width="200" height="200" alt=""/></td>
+        <td><img src="img/Olivia.jpg" width="200" height="200" alt=""/></td>
+        <td><img src="img/Ashley.jpg" width="200" height="200" alt=""/></td>
+      </tr>
+      <tr>
+        <td>JAMES COLLINS</td>
+        <td>PETER DAVIS</td>
+        <td>OLIVIA WILDE</td>
+        <td>ASHLEY GREENE</td>
+      </tr>
+      <tr>
+        <td>CEO / Marketing Guru</td>
+        <td>Creative Director</td>
+        <td>Lead Designer</td>
+        <td>SEO / Developer</td>
+      </tr>
+      <tr>
+        <td>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna.</td>
+        <td>Duis aute irure dolor in in voluptate velit esse cillum dolore fugiat nulla pariatur. Excepteur sint occaecat non diam proident.</td>
+        <td>Nemo enim ipsam voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem.</td>
+        <td>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</td>
+      </tr>
+      <tr>
+        <td>
+          <table border="0" class="four">
+            <tr>
+              <td><a href="#"><img src="img/facebook.jpg" width="8" height="15" alt=""/></a></td>
+              <td><a href="#"><img src="img/twitter.jpg" width="17" height="14" alt=""/></a></td>
+              <td><a href="#"><img src="img/link.jpg" width="16" height="16" alt=""/</a></a></td>
+              <td><a href="#"><img src="img/mail.jpg" width="16" height="12" alt=""/></a></td>
+            </tr>
+          </table>
+        </td>
+        <td>
+          <table border="0" class="three">
+            <tr>
+              <td><a href="#"><img src="img/twitter.jpg" width="17" height="14" alt=""/></a></td>
+              <td><a href="#"><img src="img/link.jpg" width="16" height="16" alt=""/></a></td>
+              <td><a href="#"><img src="img/mail.jpg" width="16" height="12" alt=""/></a></td>
+            </tr>
+          </table>
+        </td>
+        <td>
+          <table border="0" class="four">
+            <tr>
+              <td><a href="#"><img src="img/facebook.jpg" width="8" height="15" alt=""/></a></td>
+              <td><a href="#"><img src="img/twitter.jpg" width="17" height="14" alt=""/></a></td>
+              <td><a href="#"><img src="img/link.jpg" width="16" height="16" alt=""/></a></td>
+              <td><a href="#"><img src="img/mail.jpg" width="16" height="12" alt=""/></a></td>
+            </tr>
+          </table>
+        </td>
+        <td>
+          <table border="0" class="three">
+            <tr>
+              <td><a href="#"><img src="img/facebook.jpg" width="8" height="15" alt=""/></a></td>
+              <td><a href="#"><img src="img/twitter.jpg" width="17" height="14" alt=""/></a></td>
+              <td><a href="#"><img src="img/mail.jpg" width="16" height="12" alt=""/></a></td>
+            </tr>
+          </table>
+        </td>
+      </tr>
+    </table>
+    
+    </div>
+  <div id="skills">
+    <h2>WE GOT SKILLS!<hr style="width:90px; color:#d6e1e1;"></h2>
+    <p style="margin-bottom:70px; font-size:12px;"><b>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod<br> tempor incididunt ut labore et dolore magna aliqua.</b></p>
+    <table width="260" border="0" align="center">
+      <tr>
+        <td><img src="img/90.jpg" width="160" height="210" alt=""/></td>
+        <td><img src="img/75.jpg" width="160" height="211" alt=""/></td>
+        <td><img src="img/70.jpg" width="189" height="210" alt=""/></td>
+        <td><img src="img/85.jpg" width="160" height="211" alt=""/></td>
+      </tr>
+    </table>
+    
+    </div>
+  <div id="portfolio">
+    <h2 style="color:#fff;">OUR PORTFOLIO<hr style="width:90px;"></h2>
+    <p style="margin:30px 0 120px 0; font-size:12px; color:#fff;"><b>Neque porro quisquam est, qui dolorem ipsum quia dolor sit<br>amet consectetur, adipisci velit, sed quia non numquam</b></p>
+    <div class="categories">
+      <ul>
+        <li><a href="index.php">ALL</a></li>
+        <li><a href="#">WEB</a></li>
+        <li><a href="#">APPS</a></li>
+        <li><a href="#">ICONS</a></li>
+      </ul>
+    </div>
+    <div id="works">
+      <table width="1440" border="0" class="works">
+        <tr>
+          <td><img src="img/1.jpg" width="480" height="480" alt=""/></td>
+          <td><img src="img/2.jpg" width="480" height="480" alt=""/></td>
+          <td><img src="img/3.jpg" width="480" height="480" alt=""/></td>
+        </tr>
+        <tr>
+          <td><img src="img/4.jpg" width="480" height="480" alt=""/></td>
+          <td><img src="img/5.jpg" width="480" height="480" alt=""/></td>
+          <td><img src="img/6.jpg" width="480" height="480" alt=""/></td>
+        </tr>
+      </table>
+    </div>
+    </div>
+  <div id="testmonials">
+    <h2 style="color:#fff;">WHAT POEPLE SAY ABOUT US<hr style="width:90px;"></h2>
+    <p style="margin-bottom:70px; font-size:12px; color:#fff;">Our clients love us!</p>
+    <table width="1100" border="0" class="people">
+      <tr class="people1">
+        <td>
+          <table width="540" border="0" class="prof1">
+            <tr>
+              <td><img src="img/Chanel.jpg" width="96" height="96" alt=""/></td>
+              <td>Nullam dapibus blandit orci, viverra gravida dui lobortis eget. Maecenas fringilla urna eu nisl scelerisque.</td>
+            </tr>
+            <tr>
+              <td></td>
+              <td>CHANEL IMAN</td>
+            </tr>
+            <tr>
+              <td></td>
+              <td>CEO of Pinterest</td>
+            </tr>
+            </table>
+        </td>
+        <td>
+          <table width="540" border="0" class="prof2">
+            <tr>
+              <td><img src="img/Brooklyn.jpg" width="96" height="96" alt=""/></td>
+              <td>Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta.</td>
+            </tr>
+            <tr>
+              <td></td>
+              <td>BROOKLYN JONES</td>
+            </tr>
+            <tr>
+              <td></td>
+              <td>Founder of Instagram </td>
+            </tr>
+            </table>
+        </td>
+      </tr>
+      <tr class="people2">
+        <td>
+          <table width="540" border="0" class="prof3">
+            <tr>
+              <td><img src="img/Donald.jpg" width="96" height="96" alt=""/></td>
+              <td>Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta.
+              </td>
+            </tr>
+            <tr>
+              <td></td>
+              <td>DONALD ROBERTS</td>
+            </tr>
+            <tr>
+              <td></td>
+              <td>Lead Designer at Behance </td>
+            </tr>
+            </table>
+        </td>
+        <td>
+          <table width="540" border="0" class="prof4">
+            <tr>
+              <td><img src="img/Joe.jpg" width="96" height="96" alt=""/></td>
+              <td>Phasellus non purus vel arcu tempor commodo. Fusce semper, purus vel luctus molestie, risus sem cursus neque.
+              </td>
+            </tr>
+            <tr>
+              <td></td>
+              <td>JOE WARD</td>
+            </tr>
+            <tr>
+              <td></td>
+              <td>Co-Founder of Shazam</td>
+            </tr>
+            </table>
+        </td>
+      </tr>
+    </table>
+    
+    </div>
+  <div id="get">
+    <h2 style="color:#000;">GET IN TOUCH<hr style="width:90px;"></h2>
+    <p style="margin-bottom:70px; font-size:12px; color:#000;">1600 Pennsylvania Ave NW, Washington, DC 20500, United States of America. Tel: (202) 456-1111</p>
+    <div>
+      <form>
+        <input type="name" value="Your Name" maxlength="30" size="40" style="height:20px; color:#a2a2a2; padding:15px;">&nbsp;&nbsp;&nbsp;<input type="email" value="Your Email" maxlength="30" size="40" style="height:20px; color:#a2a2a2; padding:15px;"><br><br>
+        <input type="text" value="Your Message" maxlength="200" size="95" style="height:200px; color:#a2a2a2;"></input>
+      </form><br><br>
+      <img src="img/send.jpg" width="293" height="70" alt=""/>
+    </div>
+    <div id="Base" style="height:210px; background:#e7e7e7; margin-top:178px; padding-bottom:65px;"><p style="color:#b2b2b2; padding:55px;">FOLLOW US</p>
+      <div class="icons"><img src="img/icons.jpg" width="350" height="44" alt=""/></div>
+    </div>
+    </div>
+</div>
+</body>
+</html>
